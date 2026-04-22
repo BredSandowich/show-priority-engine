a0a43c65ee78f70d5d0468f6d73c16ef3cb8d609
BredSandowich <192383906+BredSandowich@users.noreply.github.com>
2026-04-22 17:20:58 -0600

    Added dataset and csv import to main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 # main.py
+import csv
+
+def load_shows(filename):
+    shows = []
+    with open(filename, mode='r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            shows.append(row)
+    return shows
+
+
 def main():
+    data = load_shows("shows.csv")
+    print(f"Loaded {len(data)} shows from your list!")
+    
     print("--- Welcome to the Show watch priority engine ---")
+    
+    if data:
+        print(data[0])
     
 
 if __name__ == "__main__":
