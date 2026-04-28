686246b2b65370f1e3383a0280ddb2d2d9ac8829
BredSandowich <192383906+BredSandowich@users.noreply.github.com>
2026-04-27 19:52:53 -0600

    Added logic for weighted average ranking

---
+++ logic.py
@@ -0,0 +1,15 @@
+# logic.py
+
+#Field names (Constants)
+field_network = "network"
+field_show = "show"
+field_rating = "rating"
+
+#Scoring weights from internet (adjust as needed)
+weight_imdb = 0.5
+weight_rottenT = 0.3
+weight_other = 0.2
+
+#Multipliers (adjust as needed)
+friend_recommendation_multiplier = 2
+mood_multiplier = 0.7
